fa3eba5d619539690db9bab44c4a2471af8cebe4
greg-beach <gbeach22@gmail.com>
2025-07-02 13:16:11 -0400

    initiated project to read/write to a json file

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
-func main(
+import (
+	"fmt"
+	"log"
+
+	"github.com/greg-beach/blog_aggregator/internal/config"
 )
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("greg")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
+}
