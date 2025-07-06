fb15621c86408ee3bd966abdc02521dc8d827ef0
greg-beach <gbeach22@gmail.com>
2025-07-06 10:18:16 -0400

    beginning to implement sql database and queries, creating a register handler

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/greg-beach/blog_aggregator/internal/config"
+	"github.com/greg-beach/blog_aggregator/sql/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,7 +21,15 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Error opening sql db")
+	}
+
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:  &dbQueries,
 		cfg: &cfg,
 	}
 
@@ -37,4 +49,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
